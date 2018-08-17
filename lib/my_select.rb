7c3ee9d161d7e800eee8ffce5484595279eed267
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 16:40:25 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(collection) 
+count = 0
+result = []
+
+while count < collection.size
+  if yield(collection[count]) == true
+  result.push(collection[count])
 end
+  count+=1
+end
+result
